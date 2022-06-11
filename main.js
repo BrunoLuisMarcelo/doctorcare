ca3444ae73ab3847efd52c712269b93b3065b886
Bruno Luis Marcelo <103146764+BrunoLuisMarcelo@users.noreply.github.com>
2022-06-11 20:25:09 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,78 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+function onScroll() {
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section) {
+const targetLine = scrollY + innerHeight / 2
+
+
+const sectionTop = section.offsetTop
+const sectionHeigth = section.offsetHeight
+
+const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+const sectionEndsAt = sectionTop + sectionHeigth
+
+const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+const sectionBoundaries = 
+sectionTopReachOrPassedTargetline && 
+!sectionEndPassedTargetline
+
+const sectionId = section.getAttribute('id')
+const menuElement = document
+.querySelector(`.menu a[href*=${sectionId}]`)
+
+
+menuElement.classList.remove('active')
+if (sectionBoundaries) {
+  menuElement.classList.add('active')
+}
+
+}
+
+function showNavOnScroll(){
+  if (scrollY > 0){ 
+    navegacao.classList.add('scroll')
+   }else{
+     navegacao.classList.remove('scroll')
+   }
+}
+
+function showBackToTopButtonOnScroll(){
+  if (scrollY > 500){ 
+    backToTopButton.classList.add('show')
+   }else{
+    backToTopButton.classList.remove('show')
+    }
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+}).reveal(`
+#home,
+#home img,
+#home .stats,
+#services,
+#services header,
+#services .card,
+#about,
+#about header,
+#about .content`);
